f7509599d8bc4a5aee70a1bc0852b34b20a9fa8e
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-05-24 10:37:28 +0000

    .....

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,4 +1,4 @@
-[2:55 pm, 24/05/2022] Sindhu c n Revaaa: # Functions
+# Functions
 
 def computepay(hrs, r):
    if hrs<40:
@@ -9,33 +9,5 @@ def computepay(hrs, r):
 hrs = float(input())
 r= float(input())
  
-p = computepay(hrs, r
+p = computepay(hrs, r)
 print("Pay", p)
-[2:56 pm, 24/05/2022] Sindhu c n Revaaa: # Loops & Iterators
-large=None
-small=None
-while True:
-    n =input()
-    if (n=="done"):
-        break
-    try:
-      num = int(n)
-        
-    except:
-        print("Invalid input")
-        continue
-        
-    if small is None:
-        small = num  
-    elif(small>num):
-        small=num
-        
-        
-    if large is None:
-        large = num
-    elif(large<num):
-        large=num
-        continue
-        
-print("Maximum is",large)
-print("Minimum is",small)
