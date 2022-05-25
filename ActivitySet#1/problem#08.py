d9cf147c1e3ae4701b6fb6c7bf703496ebd0c803
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-05-25 19:28:06 +0000

    .....

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,13 @@
 # Files
-
-filename = "dataset/mbox-short.txt"
+# Use the file name mbox-short.txt as the file name
+#filename = "dataset/mbox-short.txt"
+name=input("Enter the file name: ","r")
+fhand=open(name)
+sm=0
+count=0
+fread=fhand.readlines()
+for line in fread:
+    if line.startswith("X-DSPAM-Confidence:"):
+        sm+=float(line.split()[1])
+        count+=1
+print(f"Average spam confidence: {sm/count}")     
