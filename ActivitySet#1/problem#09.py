1931cebf558784570af9221370b8d3c2bc4a1cfa
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-05-27 04:51:17 +0000

    .....

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,61 @@
 # Lists
+#method-1
 
 filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line=line.strip()
+    words=line.split()
+    for nword in words:
+        if not nword in lst:
+            lst.append(nword)       
+lst.sort()
+print(lst)
+
+# or
+#method-2
+
+#fname = input("Enter file name: ")
+#fh = open(fname)
+#lst = list()
+#for line in fh:
+#    words=line.strip().split()
+#    for nword in words:
+ #       if not nword in lst:
+  #          lst.append(nword)       
+#lst.sort()
+#print(lst)
+
+#or
+#method-3
+
+#fname = input("Enter file name: ")
+#fh = open(fname)
+#lst = list()
+#for line in fh:
+ #   words=line.strip().split()
+ #   for nword in words:
+#        if nword in lst:
+ #           continue
+  #      else:
+   #         lst.append(nword)       
+#lst.sort()
+#print(lst)
+
+#or
+#method-4
+
+#fname = input("Enter file name: ")
+#fh = open(fname)
+#lst = list()
+#for line in fh:
+#    words=line.strip().split()
+#    for nword in words:
+#        if nword in lst:
+#            continue
+#        elif nword not in lst:
+#            lst.append(nword)
+#lst.sort()
+#print(lst)   
