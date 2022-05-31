25ff31c4c39d649b17aa7a4729b074164b3afedf
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-05-31 09:34:35 +0000

    .....

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,21 @@
 # Tuples
+fname = input("Enter file name: ")
+    
+try:    
+    fh = open(fname)
+except:
+    print("file not exist")
 
-filename = "dataset/mbox-short.txt"     
+count = 0
+
+for line in fh:
+    
+	if line.startswith("From "):
+		words = line.split()
+		count = count+1
+		print(words[1])
+	else:
+		continue
+                 
+
+print("There were", count, "lines in the file with From as the first word")
