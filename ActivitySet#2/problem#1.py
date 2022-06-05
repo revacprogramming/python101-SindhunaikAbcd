f291dff32b0a6d20d09a5e9a4ce1c959f5c811aa
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-06-05 15:08:40 +0000

    .....

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,1 +1,12 @@
-hwqiouriuqwueu09q309ruu09qu09eu
+
+def add(a, b):
+    return(a+b)
+
+
+def man():
+    a = int(input("Enter the first number :"))
+    b = int(input("Enter the second number :"))
+    c = add(a, b)
+    print(c)
+  
+man()
