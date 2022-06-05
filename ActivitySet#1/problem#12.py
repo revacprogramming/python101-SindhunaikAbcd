6604ef2b1519c97ee6ee90c4a042030344557211
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-06-05 16:04:55 +0000

    .....

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,12 @@
-a
-b
+# Regular Expressions
+# https://www.py4e.com/lessons/regex
+import re
+a=input("Enter the file name: ")
+b=open(a)
+c=re.findall(["[0-9]+",b.read()])
+for i in c:
+    print(sum(int(c)))
+
+#short form
+#import re
+#print(sum([int(i) for i in re.findall("[0-9]+",open(name).read())]))  
