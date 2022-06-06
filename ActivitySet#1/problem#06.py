4a1f84f27d1c10a5e1769cbcd75ac6abdf0dbc50
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-06-06 16:52:24 +0000

    ....

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,13 +1,2 @@
-# Loops & Iterators
-
-def computepay(h, r):
-    if h<=40:
-        value=h*r
-    else:
-        value=40*r + (h-40)*r*1.5
-    return value
-
-hrs = int(input("Enter Hours:"))
-rate= float(input("Enter rate:"))
-p = computepay(hrs, rate)
-print("Pay", p)
+s
+i
