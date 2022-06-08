b69bea90d10244dc7f876009923b69a9fb0608f4
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-06-08 17:32:37 +0000

    .....

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,1 +1,22 @@
-bbddhsfriulfywfryf
+# Lists
+
+filename = "mbox-short.txt"
+count = 0
+total = 0
+fname = input("Enter file name: ")
+try:
+	fh = open(fname)
+except:
+  	print("file is not found")
+
+for line in fh:
+	if not line.startswith("X-DSPAM-Confidence:"):
+		continue
+	else:
+		count = count+1
+		x = line.find(":")
+		y = float(line[x+1:])
+		total = total + y
+		
+avg = total/count            
+print("Average spam confidence:",avg)
