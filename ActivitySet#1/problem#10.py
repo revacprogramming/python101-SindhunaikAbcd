fd8d078b8a98ddb2475f82ae4762baef6ae75e50
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-06-08 17:36:14 +0000

    .....

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,1 +1,13 @@
-gduyfj8tfyo;9t8yo9o8t89naknsfnbegkjnd8494utmf
+# Dictionaries
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    words_split = line.split()
+    for word in words_split:
+        if word not in lst:
+            lst.append(word)
+
+     
+lst.sort()
+print(lst)
