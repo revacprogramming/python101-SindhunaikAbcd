dc1e533e614a8781f8960307a888785c36798838
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-06-09 15:36:36 +0000

    .....

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,1 +1,8 @@
-tkmhtrpohkkeykok[o]
+# Using Web Services
+# https://www.py4e.com/lessons/servces
+import re
+a=input("Enter the file name: ")
+b=open(a)
+c=re.findall("[0-9]+", b.read())
+sum=sum([int(i) for i in c])
+print(sum)
