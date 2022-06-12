1523c44730b0db4f2568eebbba6dd1f194069274
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-06-12 16:37:45 +0000

    .....

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,1 +1,24 @@
-sindhunaik31mffksi0j9t49tkg0it-0t=rdf,togogmewl],fx,tgkmkyijoyhhhjjhhjkfttioooooooooooooooombbmmeoifgle.d,vmtoomfgb
+# Network Programming
+# https://www.py4e.com/lessons/network
+filename = input("Enter filename:")
+filename = "mbox-short.txt"
+handle = open(filename)
+d = dict()
+lst = list()
+
+for line in handle:
+    if line.startswith("From "):
+        line=line.split()
+        line=line[5]
+        line=line[0:2]
+        d[line]=d.get(line,0)+1
+    else:
+            continue
+            
+for key,Value in  d.items():
+    lst.append((key,Value))
+
+lst.sort()
+    
+for key,Value in lst[:]:
+    print(key,Value)
