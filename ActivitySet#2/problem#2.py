f239980a7e1a8b0677f8ae325ea2f49390006899
SindhunaikAbcd <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-07-01 10:08:14 +0530

    Update problem#2.py
    done

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,1 +1,14 @@
-aaa
+def add(a, b):
+    sum=a+b
+    return(sum)
+    
+def output(a, b, sum):
+    print(f"sum of {a}+{b}={sum}")#f will not read it as string
+  
+def main():
+    a, b = map(int,input('input? ').split())
+    sum = add(a, b)
+    output(a, b, sum)
+
+if __name__ == '__main__':
+    main()
