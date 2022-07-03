c99294f24cee709783d55d3f65876af9671ef02b
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-07-03 04:37:14 +0000

    .....

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,5 +1,1 @@
-# Variables, Expressions & Statements
-hrs = int(input("Enter Hours:"))
-rate = float(input("enter rate:"))
-pay = hrs*rate
-print("pay",pay)
+sindhu naik 31 mam place give me the attendence
