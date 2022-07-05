a357b6fee89cd26a2be928ba10112d23596e8d0c
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-07-05 10:41:02 +0000

    Done

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,15 +1,21 @@
-# Databases
-# https://www.py4e.com/lessons/database
-import sqlite3  #here u r importing sqlite3
-con = sqlite.connect('emaildb.sqlite') #connecting to requite email that u want to analyse
-cur = con.cursor()  #we send sql commands through cursor
-
-cur.excecute('Drop a table if exists')
-cur.excecute('''
-CREATE TABLE Counts(email TEXT,count INTEGER)''')...
-
-
-
-
-sindhu c naik the name the enter the printf 
-enter the name #entter the name  printf theb na,ev
+class car:
+    def __init__(self,year,company,model,speed,price):
+        self.year=year
+        self.company=company
+        self.model=model
+        self.speed=speed
+        self.price=price
+    def display_car_details(self):
+        print(self.year,"\t",self.company,"\t",self.model,"\t",self.speed,"\t",self.price)
+carobject=[]
+n=int(input("How many car details you want to enter"))
+for i in range(n):
+    year=int(input("Enter Manufacturing year\n"))
+    company=input("Enter Manufacturing Company\n")
+    model=input("Enter Model\n")
+    speed=float(input("Enter the speed\n"))
+    price=float(input("Enter the price of Car\n"))
+    carobject.append(car(year,company,model,speed,price))
+print("Car Details are\n")
+for obj in carobject:
+    obj.display_car_details()
